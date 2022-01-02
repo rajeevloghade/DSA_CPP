52c9b4801ab735a0c47e6f712f56ae7598fd8a8c
rajeevloghade <rajeevloghade010@gmail.com>
2022-01-02 23:04:05 +0530

    Print Like WAVE

---
+++ Questions/2D Array Basic to Advance/PrintLikeWAVE.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printLikeWave(int arr2D[][4], int nRows, int mCols)
+{
+    vector<int> ans;
+
+    for (int col = 0; col < mCols; col++)
+    {
+        if (col & 1)
+        {
+            for (int row = nRows - 1; row >= 0; row--)
+                cout << arr2D[row][col] << " ";
+            //    ans.push_back(arr[row][col]);
+        }
+        else
+        {
+            for (int row = 0; row < nRows; row++)
+                cout << arr2D[row][col] << " ";
+            // ans.push_back(arr[row][col]);
+        }
+    }
+    // return ans;
+}
+int main()
+{
+    int arr2D[3][4]; // 2D arr2D[row][column]
+
+    cout << "Please enter elements : ";
+    //taking inputs (row wise) for 2D array
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            cin >> arr2D[i][j];
+
+    printLikeWave(arr2D, 3, 4);
+    return 0;
+}
