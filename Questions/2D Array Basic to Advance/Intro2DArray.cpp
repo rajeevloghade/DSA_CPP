9021f81a6362ac5948ee317e100724a90e78d6ee
rajeevloghade <rajeevloghade010@gmail.com>
2022-01-07 17:28:01 +0530

    Intro2DArray

--- Questions/2D Array Basic to Advance/Intro2DArray.cpp
+++ Questions/2D Array Basic to Advance/Intro2DArray.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+print2DVector(vector<vector<int>> array)
+{
+    for (int i = 0; i < array.size(); i++)
+    {
+        for (int j = 0; j < array[i].size(); j++)
+            cout << array[i][j] << " ";
+        cout << endl;
+    }
+}
 
 int main()
 {
@@ -11,10 +22,10 @@ int main()
         for (int j = 0; j < 4; j++)
             cin >> arr2D[i][j];
 
-    //taking inputs (column wise) for 2D array
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 3; j++)
-            cin >> arr2D[j][i];
+    // //taking inputs (column wise) for 2D array
+    // for (int i = 0; i < 4; i++)
+    //     for (int j = 0; j < 3; j++)
+    //         cin >> arr2D[j][i];
 
     //displaying 2D array
     for (int i = 0; i < 3; i++)
@@ -23,5 +34,23 @@ int main()
             cout << arr2D[i][j] << " ";
         cout << endl;
     }
+
+    //creating 2D vector
+    vector<vector<int>> array; // = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    //taking inputs (row wise) for 2D vector
+    int val;
+    for (int i = 0; i < 3; i++)
+    {
+        vector<int> two;
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> val;
+            two.push_back(val);
+        }
+        array.push_back(two);
+    }
+
+    print2DVector(array);
     return 0;
 }
